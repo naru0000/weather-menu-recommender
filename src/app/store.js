f1fb4cfdbf87447d73529baba8eb8fd9ee6e0a11
woori ko <mynameiswooriko@gmail.com>
2025-08-22 15:00:50 +0900

    fix: import 경로 수정하여 store.js 파일의 모듈 불러오기 개선

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import weatherSlice from './features/weather/weatherSlice'
-import temperatureSlice from './features/temperature/temperatureSlice'
-import menuSlice from './features/menu/menuSlice'
-import historySlice from './features/history/historySlice'
+
+import weatherSlice from '../features/weather/weatherSlice'
+import temperatureSlice from '../features/temperature/temperatureSlice'
+import menuSlice from '../features/menu/menuSlice'
+import historySlice from '../features/history/historySlice'
 
 export const store = configureStore({
     reducer: {
