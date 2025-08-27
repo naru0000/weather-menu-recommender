bc33dab3fc4c9bd414a0f50db33945ff3c5ec0d6
최진호 <chlwlsgh777@gmail.com>
2025-08-27 16:17:36 +0900

    기온 Slice store연결까지 완료

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        {/* <Provider store={store}> */}
+        <App />
+        {/* </Provider> */}
     </StrictMode>
 )
